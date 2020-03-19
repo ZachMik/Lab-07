7c45a7da2bb88e5316f0a916f4fa57221e0f500c
Zach Miklitsch <z@Zachs-MBP.fios-router.home>
2020-03-19 19:18:12 -0400

    add newFile2

---
+++ newFile2.py
@@ -0,0 +1,8 @@
+userName = input("Hello! Please enter your facebook username!")
+userPassword = input("Hello! Please enter your facebook password!")
+
+if(userName.lower() == "zach" and userPassword == "Password"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
+
